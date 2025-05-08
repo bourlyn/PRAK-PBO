eaa797bcbf3fffab2730287e840f5a41a346090c
bourlyn <zbourlyn@gmail.com>
2025-05-08 15:54:56 +0700

    Add files via upload

--- W7_latihan/Latihan/src/Main.java
+++ W7_latihan/Latihan/src/Main.java
@@ -1,38 +1,44 @@
-// Latihan NO1
-class Hewan { // parent
-    String suara = "Suara hewan";
-}
+// parent class (superclass)
+class Kendaraan {
+    String Merek = "Toyota";
 
-class Kucing extends Hewan { // child
-    String suara = "Meong";
-    
-    void dispalySuara() {
-        System.out.println("suara milik kelas Kucing: " + suara);
-        System.out.println("suara milik kelas Hewan: " + super.suara);
-    }
-}
-
-// Latihan NO2
-class Kendaraan {// parent
     Kendaraan() {
-        System.out.println("Konstruktor Kendaraan dipanggil");
+        System.out.println("ini konstruktor Parent");
     }
+
+    void jalan() {
+        System.out.println("kendaraan sedang berjalan");
+    }
+ 
 }
 
+// child class (subclass)
 class Mobil extends Kendaraan {
-    Mobil() { // child
-        super(); 
-        System.out.println("Konstruktor Mobil dipanggil");
+    String Merek = "BMW";
+
+    public Mobil() {
+        super();
+        System.out.println("ini konstruktor Child");
+    }
+
+    void printInfo(){
+        System.out.println("Merek mobil child " + Merek);
+        System.out.println("Merek mobil parent " + super.Merek);
+        jalan();
+    }
+    
+    void klakson(String Merek) {
+        System.out.println("Mobil " + super.Merek + " sedang klakson ");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        // NO1
-        Kucing kucing = new Kucing();
-        kucing.dispalySuara();
-        // NO2
-        Mobil mobil = new Mobil();
+        Mobil jazz = new Mobil();
+        // jazz.jalan();
+        jazz.printInfo();
+        jazz.klakson(" BMW ");
+        System.out.println(jazz.Merek);
     }
 }
 
